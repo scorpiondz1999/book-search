84a2d75d10342fe6d378e2d9b8cd4386cb55d0b9
Djamel Ramdani <enzonpapa@gmail.com>
2022-02-20 17:04:47 -0600

    deployed to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://scorpiondz1999:Enzoram@cluster0.7xv8k.mongodb.net/bookSearch?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
